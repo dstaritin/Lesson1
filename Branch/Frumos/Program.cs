f836a5a5cfddc0144c1af7babf5d7b73b94a2b5e
denis.staritin(VS) <dstaritin@gmail.com>
2016-06-21 15:10:28 +0300

    He isn't right

--- Branch/Frumos/Program.cs
+++ Branch/Frumos/Program.cs
@@ -11,6 +11,8 @@ namespace Frumos
         static void Main(string[] args)
         {
             Console.WriteLine("Squares of numbers");
+
+            Console.WriteLine("0 - 0\n1 - 1\n2 - 4\n3 - 9\n 4 - 16\n5 - 25\n6 - 36\n7 - 49\n8 - 64\n9 - 81");
         }
     }
 }
