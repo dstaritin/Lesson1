4bd1f309b70c96adb0655c9b18291a5e0b129d58
denis.staritin(VS) <dstaritin@gmail.com>
2016-06-21 14:05:01 +0300

    One more time

---
+++ Branch/Branch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smthg");
+            Console.WriteLine("Else");
+        }
+    }
+}
