b26fc81d8fc077696ef48d82a7edd70b6803c08e
denis.staritin(VS) <dstaritin@gmail.com>
2016-06-21 13:19:32 +0300

    Commit from VS

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+            Console.WriteLine("100 / 3 = {0}", i /= 3);
+        }
+    }
+}
